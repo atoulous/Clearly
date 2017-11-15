bea583421fe89aa78e2eb8b366c3182a2e84c65f
Aymeric TOULOUSE <atoulous@e1r5p9.42.fr>
2017-11-15 15:06:51 +0100

    15nov17

--- 12_recursive_file_script/Ressources/recursive_read.js
+++ 12_recursive_file_script/Ressources/recursive_read.js
@@ -1,40 +1,38 @@
 const axios = require('axios');
 const _ = require('lodash');
-const fs = require('fs');
-const path = require('path');
 
-const base = 'http://192.168.202.128/.hidden';
+const base = 'http://10.11.200.171/.hidden/whtccjokayshttvxycsvykxcfm/';
 const regex = /<a href="(.+)">/;
 const regex2 = /^[a-zA-Z0-9]{6,}/;
+const regex3 = /^[a-zA-Z0-9]{20,}/;
 
 const recursiveSearch = async (url) => {
-	if (url) {
-		try {
-			const req = await axios.get(url);
-			const array = req.data.split(regex);
-			let list = _.map(array, (e) => {
-				if (e) {
-					const match = e.match(regex2);
-					if (match && match.length) return match[0];
+	try {
+		const req = await axios.get(url);
+		const array = req.data.split(regex);
+		let list = _.map(array, (e) => {
+			if (e) {
+				const match = e.match(regex2);
+				if (match && match.length) return match[0];
+			}
+		});
+		list = _.uniqBy(list, (e) => e);
+		_.map(list, async (e) => {
+			if (e === 'README') {
+				uri = `${url}/${e}`;
+				const req2 = await axios.get(uri);
+				const res = req2.data.toString();
+				if (res.match(regex3)) {
+					console.log('flag is', res);
+					process.exit();
 				}
-			});
-			list = _.uniqBy(list, (e) => e);
-			_.map(list, async (e) => {
-				if (e === 'README') {
-					uri = `${url}/${e}`;
-					//console.log('URL1==', uri);
-					const req2 = await axios.get(uri);
-					const res = req2.data.toString();
-					console.log('res==', res);
-				} else if (e && e !== undefined && e !== '../') {
-					//console.log('nurl==', e);
-					await recursiveSearch(`${url}/${e}`);
-				}
-			});
-		} catch (err) { throw err }
-	}
+			} else if (e && e !== undefined && e !== '../') {
+				await recursiveSearch(`${url}/${e}`);
+			}
+		});
+	} catch (err) { throw err }
 }
 
 recursiveSearch(base)
-	.then()
+	.then(() => console.log('Script running'))
 	.catch((err) => console.log('err==', err));
